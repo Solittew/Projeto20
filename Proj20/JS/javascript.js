c41ac563d705df79319032d11226509d9ca02c32
Solittew <129474112+Solittew@users.noreply.github.com>
2023-11-20 21:41:14 -0300

    atualização
    mudanças no index, css e a nova pasta JS

---
+++ Proj20/JS/javascript.js
@@ -0,0 +1,18 @@
+function RedirecionarSite(url){
+    window.location.href = "marcos1.html";
+}
+function RedirecionarSite1(url){
+    window.location.href = "marcos1.html";
+}
+function RedirecionarSite2(url){
+    window.location.href = "marcos1.html";
+}
+function RedirecionarSite3(url){
+    window.location.href = "marcos1.html";
+}
+function RedirecionarSite4(url){
+    window.location.href = "marcos1.html";
+}
+function RedirecionarSite5(url){
+    window.location.href = "marcos1.html";
+}
