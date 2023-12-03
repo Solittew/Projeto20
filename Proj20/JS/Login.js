a7d2c9fe9614274bb434c88f40f4343cce764924
Solittew <129474112+Solittew@users.noreply.github.com>
2023-12-03 13:58:14 -0300

    Ajax no login

--- Proj20/JS/Login.js
+++ Proj20/JS/Login.js
@@ -1,4 +1,54 @@
 function Login() {
+    var senha = $('#SENHA').val();
+    var email = $('#EMAIL').val();
+
+    function validarEmail(email) {
+        var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(email);
+    }
+    function validarSenha(senha) {
+        return senha !== "";
+    }
+
+    if(validarEmail(email) && validarSenha(senha)){
+        $.ajax({
+            method: 'get',
+            url:'Login.php',
+            data: { 'EMAIL': email, 'SENHA': senha }, // Corrigido aqui
+    
+            beforeSend: function () { // Corrigido aqui
+                $('#Aguardo').html("Aguarde...");
+            }
+        })
+    
+        .done (function(RETORNOPHP){
+
+            var Conta = JSON.parse(RETORNOPHP);
+
+            if(Conta.EMAIL == email && Conta.SENHA == senha){
+                $('#Aguardo').html("Login efetuado com sucesso!");
+                window.location.href = "index.html"
+            }
+            else{
+                alert("USuario incorreto");
+            }
+
+
+
+
+        })
+    
+        .fail(function(){
+            $('#Aguardo').html("Erro tente novamente");
+        })
+    }
+    else{
+        alert("Email ou senha invalidos")
+    }
+
+    return false;
+}
+/*    
     function validarEmail(email) {
         var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return regex.test(email);
@@ -16,4 +66,8 @@ function Login() {
     } else {
         alert("Email ou senha inválidos");
     }
-}
+    
+    
+    
+
+    */
